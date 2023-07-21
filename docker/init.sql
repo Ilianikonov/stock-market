d1830dc830f36101059e7ec69a9ab1bf8e99ce19
Ilianikonov <nikonov.ilia2013@yandex.ru>
2023-07-21 18:52:45 +0300

    тесты

--- docker/init.sql
+++ docker/init.sql
@@ -8,15 +8,103 @@ create table trader
     password varchar(30) not null
 );
 
-create table transaction
+create table transaction_type
 (
-    id            serial primary key,
-    currency_name varchar(10)      not null,
-    amount        double precision not null,
-    trader_id     int references trader (id)
+    id   serial primary key,
+    name varchar(10) not null
 );
 
+create table transaction
+(
+    id                 serial primary key,
+    currency_name_from varchar(10)      not null,
+    currency_name_to   varchar(10),
+    amount             double precision not null,
+    trader_id          int references trader (id),
+    type_id            int references transaction_type (id),
+    commission         double precision not null
+);
+
+INSERT INTO transaction_type (name)
+values ('DEPOSITING');
+INSERT INTO transaction_type (name)
+values ('WITHDRAWAL');
+INSERT INTO transaction_type (name)
+values ('EXCHANGE');
 INSERT INTO trader (name, password)
 values ('nikonov_ilia', 'nikonov_ilia');
-INSERT INTO transaction (amount, trader_id, currency_name)
-values (100, 1, 'RUB');
+INSERT INTO transaction (amount, trader_id, currency_name_from, type_id, commission)
+values (
+        660,
+        (select id from trader where name = 'nikonov_ilia'),
+        'RUB',
+        (select id from transaction_type where name = 'DEPOSITING'),
+        10.0
+        );
+INSERT INTO transaction (amount, trader_id, currency_name_from, type_id, commission)
+values (
+           50,
+           (select id from trader where name = 'nikonov_ilia'),
+           'RUB',
+           (select id from transaction_type where name = 'DEPOSITING'),
+           10.0
+       );
+INSERT INTO transaction (amount, trader_id, currency_name_from, currency_name_to, type_id, commission)
+values (
+           13,
+           (select id from trader where name = 'nikonov_ilia'),
+           'RUB',
+            'USD',
+           (select id from transaction_type where name = 'EXCHANGE'),
+           10.0
+       );
+INSERT INTO transaction (amount, trader_id, currency_name_from, type_id, commission)
+values (
+           160,
+           (select id from trader where name = 'nikonov_ilia'),
+           'RUB',
+           (select id from transaction_type where name = 'DEPOSITING'),
+           10.0
+       );
+INSERT INTO transaction (amount, trader_id, currency_name_from, type_id, commission)
+values (
+           170,
+           (select id from trader where name = 'nikonov_ilia'),
+           'USD',
+           (select id from transaction_type where name = 'WITHDRAWAL'),
+           13.0
+       );
+INSERT INTO transaction (amount, trader_id, currency_name_from, type_id, commission)
+values (
+           110,
+           (select id from trader where name = 'nikonov_ilia'),
+           'USD',
+           (select id from transaction_type where name = 'DEPOSITING'),
+           11.0
+       );
+INSERT INTO transaction (amount, trader_id, currency_name_from, type_id, commission)
+values (
+           600,
+           (select id from trader where name = 'nikonov_ilia'),
+           'RUB',
+           (select id from transaction_type where name = 'WITHDRAWAL'),
+           15.0
+       );
+INSERT INTO transaction (amount, trader_id, currency_name_from, currency_name_to, type_id, commission)
+values (
+           170,
+           (select id from trader where name = 'nikonov_ilia'),
+           'RUB',
+           'USD',
+           (select id from transaction_type where name = 'EXCHANGE'),
+           10.0
+       );
+INSERT INTO transaction (amount, trader_id, currency_name_to, currency_name_from, type_id, commission)
+values (
+           110,
+           (select id from trader where name = 'nikonov_ilia'),
+           'RUB',
+           'USD',
+           (select id from transaction_type where name = 'EXCHANGE'),
+           10.0
+       );
--- src/main/java/com/example/stockmarket/entity/Balance.java
+++ src/main/java/com/example/stockmarket/entity/Balance.java
@@ -5,4 +5,5 @@ import lombok.Data;
 public class Balance {
    private String currencyName;
    private double amount;
+
 }
