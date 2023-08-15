ceba85d1e656ad78ed0fab1e95b1b3c843364deb
Ilianikonov <nikonov.ilia2013@yandex.ru>
2023-08-15 20:52:59 +0300

    Добавил веб сервис для конвертации валюты

--- src/main/java/com/example/stockmarket/service/currency/WebCurrencyService.java
+++ src/main/java/com/example/stockmarket/service/currency/WebCurrencyService.java
@@ -12,14 +12,17 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class WebCurrencyService {
     private final RestTemplate restTemplate;
-    public double convert(String currencyFrom, String currencyTo, double amount) {
-        String currencyPair = currencyFrom + currencyTo;
+    public double convert(String receivedCurrency, String givenCurrency){
+        String currencyPair = receivedCurrency + givenCurrency;
         String url = "https://currate.ru/api/?get=rates&pairs="+ currencyPair + "&key=8290c72cf52d76f9350c7006a6fc9da4";
         ConvertCurrencyResponse convertCurrencyResponse = restTemplate.getForObject(url,ConvertCurrencyResponse.class);
         if (convertCurrencyResponse != null && convertCurrencyResponse.getStatus() == 200) {
-            return convertCurrencyResponse.getData().get(currencyPair) * amount;
+            return convertCurrencyResponse.getData().get(currencyPair);
         } else {
             throw new WebCurrencyServiceException();
         }
+    }
+    public double convert(String receivedCurrency, String givenCurrency, double amount) {
+        return amount * convert(receivedCurrency, givenCurrency);
     }
 }
