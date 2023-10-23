c54c97bef303fc6fee781b00d36b7a066c0950b0
Ilianikonov <nikonov.ilia2013@yandex.ru>
2023-10-23 15:44:18 +0300

    настройка Schema TraderRequest

--- src/main/java/com/ilianikonov/stockmarket/controller/request/trader/TraderRequest.java
+++ src/main/java/com/ilianikonov/stockmarket/controller/request/trader/TraderRequest.java
@@ -8,14 +8,22 @@ import java.util.List;
 @Data
 public abstract class TraderRequest {
     @Size(max = 20)
-    @Schema
+    @Schema(description = "уникальное имя трейдера",
+            name = "name",
+            type = "string")
     private String name;
     @Size(min = 6)
     @Size(max = 100)
-    @Schema
+    @Schema(description = "пароль трейдера",
+            name = "password",
+            type = "string")
     private char[] password;
-    @Schema
+    @Schema(description = "индикатор доступа",
+            name = "enabled",
+            type = "boolean")
     private boolean enabled = true;
-    @Schema
+    @Schema(description = "роли трейдера",
+            name = "roles",
+            type = "[string[]]")
     private List <String> roles;
 }
