65c9b12a9ce539022050d3cb31047bf70ccdb736
Ilianikonov <nikonov.ilia2013@yandex.ru>
2023-12-06 11:53:26 +0300

    подключил кафку

--- src/main/java/com/ilianikonov/stockmarket/config/KafkaConsumerConfig.java
+++ src/main/java/com/ilianikonov/stockmarket/config/KafkaConsumerConfig.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +12,6 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.support.LogIfLevelEnabled;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,32 +19,31 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-    @Value("${spring.kafka.consumer.group-id}")
-    private String groupId;
+
+    @Value("${spring.kafka.consumer.security.protocol}")
 
     @Bean
-    public DefaultKafkaConsumerFactory consumerFactory(){
+    public DefaultKafkaConsumerFactory consumerFactory(@Value("${spring.kafka.bootstrap-servers}") String bootstrapServers,
+                                                       @Value("${spring.kafka.consumer.group-id}") String groupId,
+                                                       @Value("${spring.kafka.consumer.security.protocol}") String securityProtocol,
+                                                       @Value("${spring.kafka.consumer.max.request.size}") String maxRequestSize,
+                                                       @Value("${spring.kafka.consumer.sasl.mechanism}") String saslMechanism,
+                                                       @Value("${spring.kafka.consumer.username}") String userName,
+                                                       @Value("${spring.kafka.consumer.password}") String password){
         Map<String, Object> props = new HashMap<>();
-//        JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class);
-//        deserializer.setRemoveTypeHeaders(false);
-//        deserializer.addTrustedPackages("*");
-//        deserializer.setUseTypeMapperForKey(true);
-//        deserializer.dontRemoveTypeHeaders();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "20971520");
+        props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, maxRequestSize);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-        props.put("sasl.mechanism", "PLAIN");
-        props.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='BZQWDS7YFXFTA6ZG' password='DCH8p1WgaVpXtqteaZmkoy3TsRv89xb84IgYc8ruiV+gnkbXlQVy3y2+j7hORprk';");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        props.put("sasl.mechanism", saslMechanism);
+        props.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='" + userName + "' password='" + password + "';");
         props.put("basic.auth.credentials.source", "USER_INFO");
-        props.put("basic.auth.user.info", "BZQWDS7YFXFTA6ZG:DCH8p1WgaVpXtqteaZmkoy3TsRv89xb84IgYc8ruiV+gnkbXlQVy3y2+j7hORprk");
+        props.put("basic.auth.user.info", userName + ":" + password);
         props.put("client.dns.lookup", "use_all_dns_ips");
         props.put("session.timeout.ms", "45000");
         return new DefaultKafkaConsumerFactory<>(props);
--- src/main/java/com/ilianikonov/stockmarket/service/KafkaService.java
+++ src/main/java/com/ilianikonov/stockmarket/service/KafkaService.java
@@ -1,20 +1,16 @@
 package com.ilianikonov.stockmarket.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaService {
-    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void sendObject(Object o, String topicName){
-        kafkaTemplate.send(topicName, o);
-    }
-    @KafkaListener(topics = "kafka-test", groupId = "web-25d3acb2-e08a-40a7-b33c-718f1c938404")
-    void listener(Object data){
-            System.out.println(data);
+    @KafkaListener(topics = "${spring.kafka.consumer.topic}", groupId = "${spring.kafka.consumer.group-id}")
+    void listener(ConsumerRecord<String,String> consumerRecord){
+            System.out.println(consumerRecord.value());
     }
 }
